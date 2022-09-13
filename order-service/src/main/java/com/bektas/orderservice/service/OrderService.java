bc39197288fa2df1b0229cf6e4d8df36756005d8
AliKa66 <kgp_mm@hotmail.com>
2022-09-13 09:24:20 +0200

    Add Eureka discovery server

--- order-service/src/main/java/com/bektas/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/bektas/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -36,9 +36,9 @@ public class OrderService {
 
         List<String> skuCodes = order.getOrderLineItems().stream().map(OrderLineItems::getSkuCode).toList();
 
-        InventoryResponse[] response = webClient
+        InventoryResponse[] response = webClientBuilder.build()
                 .get()
-                .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
                 .block();
--- product-service/src/main/java/com/bektas/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/bektas/productservice/ProductServiceApplication.java
@@ -2,9 +2,10 @@ package com.bektas.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
